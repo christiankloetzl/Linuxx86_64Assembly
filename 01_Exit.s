8ab2e79b6d526b6b3d02836b205e498ff8ea4c8e
christian <christian.kloetzl@posteo.de>
2019-04-01 21:18:33 +0200

    Use AT&T Syntax

--- 01_Exit.s
+++ 01_Exit.s
@@ -1,10 +1,8 @@
-.intel_syntax noprefix
-
 .text
 
 .global _start
 _start:
     #exit(2)
-    mov rax, 60 #Syscall #60: exit()
-    mov rdi, 2  #Exit status: 2
-    syscall     #System call
+    movq $60, %rax #Syscall #60: exit()
+    movq $2, %rdi  #Exit status: 2
+    syscall        #System call
